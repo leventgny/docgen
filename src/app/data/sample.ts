6a8fa553cf19aa8f3297e6c46ae0bcfbe6f962d0
Levent Günay <leventgny88@gmail.com>
2025-03-07 14:46:47 +0300

    sample app data.

---
+++ src/app/data/sample.ts
@@ -0,0 +1,23 @@
+import Project from "../model/ProjectInformation";
+
+export const projects: Project[] = [{
+    name: 'Proje 1',
+    description: 'Project Description 1',
+    deadline: Date.now()
+},{
+    name: 'Proje 2',
+    description: 'Project Description 2',
+    deadline: Date.now()
+},{
+    name: 'Proje 3',
+    description: 'Project Description 3',
+    deadline: Date.now()
+},{
+    name: 'Proje 4',
+    description: 'Project Description 4',
+    deadline: Date.now()
+},{
+    name: 'Proje 5',
+    description: 'Project Description 5',
+    deadline: Date.now()
+},]
