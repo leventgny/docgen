2360f9477c81bed08b135dc8a2dd92433c9fd7f0
Levent Günay <leventgny88@gmail.com>
2025-03-07 14:46:24 +0300

    system information.

---
+++ src/app/config/DocSystemInfo.ts
@@ -0,0 +1,13 @@
+import DocInfo from "../model/BaseDocInformation"
+
+export const systemInfo: DocInfo = {
+    overview: ['Base system overview information 1'],
+    requirements: ['Base requirement information 1'],
+    architectureAndDesign: ['Base architecture and design principles 1'],
+    developmentSpecs: ['Base development spec 1'],
+    qualityAssurance: ['Base quality assurance detail 1'],
+    devOps: ['Base devOps information 1'],
+    management: ['Base management information 1']
+}
+
+export default systemInfo
